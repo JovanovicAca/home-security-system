5fefdb13cd964b23d52acc0d92a7f8c37e41f62a
Aleksandar Jovanović <50550669+JovanovicAca@users.noreply.github.com>
2022-10-25 14:21:15 +0200

    Update EmailServiceImpl.java

--- HomeSecurityBackend/src/main/java/com/app/service/implementation/EmailServiceImpl.java
+++ HomeSecurityBackend/src/main/java/com/app/service/implementation/EmailServiceImpl.java
@@ -19,7 +19,6 @@ public class EmailServiceImpl {
 
 
     final String username = "bezbednost27@gmail.com";
-    final String password = "Bezbednost27!";
 
     public void sendMail(String to, String subject, String text) {
         SimpleMailMessage message = new SimpleMailMessage();
@@ -31,49 +30,4 @@ public class EmailServiceImpl {
         System.out.println("Email sent...");
     }
 }
-//    public void sendMessage(String to, String subject, String text) throws MessagingException {
-////        Properties prop = new Properties();
-////        prop.setProperty("mail.smtp.host", "smtp.gmail.com");
-////        prop.put("mail.smtp.port", "587");
-////        prop.put("mail.smtp.auth", "true");
-////        prop.put("mail.smtp.socketFactory.port", "587");
-////        prop.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
-//
-//        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
-//        mailSender.setHost("smtp.gmail.com");
-//        mailSender.setPort(25);
-//        mailSender.setUsername(username);
-//        mailSender.setPassword(password);Properties props = mailSender.getJavaMailProperties();
-//
-//        props.put("mail.transport.protocol", "smtp");
-//        props.put("mail.smtp.auth", "true");
-//        props.put("mail.smtp.starttls.enable", "true");
-//        props.put("mail.debug", "true");
-//
-//        Session session = Session.getInstance(prop,
-//                new javax.mail.Authenticator() {
-//                    protected PasswordAuthentication getPasswordAuthentication() {
-//                        return new PasswordAuthentication(username, password);
-//                    }
-//                });
-//        try{
-//            Message message = new MimeMessage(session);
-//            message.setFrom(new InternetAddress(username));
-//            message.setRecipient(Message.RecipientType.TO, new InternetAddress(to));
-//            message.setSubject("subject");
-//            message.setText("text");
-//            Transport.send(message);
-//            System.out.println("Email sent...");
-//        }catch (MessagingException e) {
-//            e.printStackTrace();
-//        }
-//        SimpleMailMessage message = new SimpleMailMessage();
-//        message.setFrom("bezbednost27@gmail.com");
-//        message.setTo(to);
-//        message.setSubject(subject);
-//        message.setText(text);
-//        mailSender.send(message);
-//
-//
-//    }
-//}
+
